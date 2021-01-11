757998b22d005eb465c9022528b747c0cc4f09cf
Onixaz <onixaz@gmail.com>
2021-01-11 12:23:46 +0200

    new improvements for starter

--- src/components/Seo/index.tsx
+++ src/components/Seo/index.tsx
@@ -9,7 +9,7 @@ const baseInfo = {
   name: 'pajustudios.eu',
   url: 'https://wwww.pajustudios.eu',
   description: 'Eu design studios',
-  keywords: `Masažas, Viso kūno masažas, Sportinis masažas, Nugaros masažas, Konsultacijos, Sveikata, Mityba, Laikysena`,
+  keywords: `Design, Web Development`,
 }
 
 const Seo: React.FC<CustomHeadProps> = () => {
@@ -20,28 +20,10 @@ const Seo: React.FC<CustomHeadProps> = () => {
       <meta charSet="utf-8" />
       <meta property="og:type" content="website" key="ogtype" />
       <meta property="og:description" content={baseInfo.description} key="ogdesc" />
-      {/* <meta
-        property="og:image"
-        content="https://aygdknricp.cloudimg.io/v7/https://epizootic-ingredien.000webhostapp.com/wp-content/uploads/2020/12/masazuotojas_romanas.jpg"
-        key="ogimage"
-      /> */}
       <meta property="og:url" content={baseInfo.url} key="ogurl" />
       <meta property="og:site_name" content={baseInfo.name} key="ogsitename" />
-
-      {/* <link rel="canonical" href={baseInfo.url} /> */}
       <meta name="author" content={baseInfo.author} />
       <meta name="keywords" content={baseInfo.keywords} />
-      {/* <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-      <meta name="msapplication-TileColor" content="#da532c" />
-      <meta name="theme-color" content="#ffffff" /> */}
-      {/* <title>
-        {title} | {baseInfo.titlePrefix}
-      </title> */}
-
-      {/* moved to a new component */}
     </Head>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,9 @@
-import Layout from '../components/Layout'
 import { NextPage } from 'next'
-import Section from '../components/Section'
-import Hero from '../components/Hero'
-import products from '../../products.json'
-import LatestProducts from '../components/Latest'
-import Categories from '../components/Categories'
 
 interface IndexPageProps {}
 
 const IndexPage: NextPage<IndexPageProps> = () => {
-  return (
-    <>
-      <Section id="Hero" height={100} width={100}>
-        <Hero />
-      </Section>
-      <Section id="Categories" height={30} width={90}>
-        <Categories />
-      </Section>
-    </>
-  )
+  return <>I'm a starter</>
 }
 
 export default IndexPage
