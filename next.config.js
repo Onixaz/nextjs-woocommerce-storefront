9d037e723deaf24596b739ee9f52471a24525dfd
Onixaz <onixaz@gmail.com>
2021-03-02 17:44:25 +0200

    Possible js bundle fixes

---
+++ next.config.js
@@ -0,0 +1,13 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+  })
+  module.exports = withBundleAnalyzer({})
+
+const DuplicatePackageCheckerPlugin = require('duplicate-package-checker-webpack-plugin')
+
+module.exports = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.plugins.push(new DuplicatePackageCheckerPlugin())
+    return config
+  },
+}
